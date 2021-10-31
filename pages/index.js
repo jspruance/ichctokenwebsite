12dff2b1774e6b5799c2a2cada90c776d9ae2b29
Jonathan Spruance <jspruance@inflightcorp.com>
2021-10-31 19:16:23 -0400

    Adding Google analytics

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,15 @@ export default function Home() {
         <title>I Can Has Cheezburger Token</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="/fonts/fonts.css"></link>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2EF9PKF797"></script>
+        <script>
+          window.dataLayer = window.dataLayer || []
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date())
+
+          gtag('config', 'G-2EF9PKF797')
+        </script>
       </Head>
 
       <main>
