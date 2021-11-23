ce117c80597b3ddd3a87c318bdee6dfd5967e6cf
Jonathan Spruance <jspruance@inflightcorp.com>
2021-11-23 09:11:05 -0500

    Ignore electron

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -5,15 +5,13 @@ let web3
 if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   // We are in the browser and metamask is running.
   window.ethereum.request({ method: "eth_requestAccounts" });
-  //web3 = new Web3(window.ethereum);
-  web3 = {}
+  web3 = new Web3(window.ethereum);
 } else {
   // We are on the server *OR* the user is not running metamask
-  // const provider = new Web3.providers.HttpProvider(
-  //   "https://mainnet.infura.io/v3/72979409ddda48758d9df4f284cdbce3"
-  // );
-  // web3 = new Web3(provider);
-  web3 = {}
+  const provider = new Web3.providers.HttpProvider(
+    "https://mainnet.infura.io/v3/72979409ddda48758d9df4f284cdbce3"
+  );
+  web3 = new Web3(provider);
 }
  
 export default web3
