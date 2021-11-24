791fd7f9014b98f05c9d960ab9f009e1bed6f425
Jonathan Spruance <jspruance@inflightcorp.com>
2021-11-24 06:29:39 -0500

    Refactor compile / abi

--- ethereum/faucet.js
+++ ethereum/faucet.js
@@ -1,8 +1,8 @@
 import web3 from './web3'
-const compiledFaucet = require('./contracts/Faucet.json')
+const faucetABI = require('./build/ethereum_contracts_Faucet_sol_Faucet.abi')
 
 const faucetContract = new web3.eth.Contract(
-    compiledFaucet.abi,
+    faucetABI,
     "0x4099E633A607F6ED211e2c82565003d6F755e75e"
   )
 
