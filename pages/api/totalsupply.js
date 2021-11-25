cc950b141139f912169525a0edff5ec162b1dd51
Jonathan Spruance <jspruance@inflightcorp.com>
2021-11-25 07:21:46 -0500

    Add totalSupply rest api route

--- pages/api/totalsupply.js
+++ pages/api/totalsupply.js
@@ -1,8 +1,8 @@
-import faucetContract from '../../ethereum/faucet'
+import tokenContract from '../../ethereum/token'
 
 export default function handler(req, res) {
   try {
-    const totalSupply = faucetContract.methods.totalSupply().call()
+    const totalSupply = tokenContract.methods.totalSupply().call()
     res.status(200).json({ totalSupply: totalSupply })
   } catch(err) {
     res.status(500).json({ error: err.message })
