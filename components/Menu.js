6745cbc36e9292fe75f9dad7e51a0d530b1ba50f
Jonathan Spruance <jspruance@inflightcorp.com>
2021-11-25 05:09:34 -0500

    Add meta tags

--- components/Menu.js
+++ components/Menu.js
@@ -18,6 +18,9 @@ export default () => {
           <Link href="/about">
             <a>About</a>
           </Link>&nbsp;|&nbsp;
+          <Link href="https://www.instagram.com/ichctoken/">
+            <a target="_blank">Instagram</a>
+          </Link>
           <span className="contact white">
             <Link href="mailto:crypto@ichctoken.org"><a>crypto@ichctoken.org</a></Link>
           </span>
