cf4c00ed64c2aef380ef7bbfae5f06b5801bc9dc
Jonathan Spruance <jspruance@inflightcorp.com>
2021-11-25 07:26:29 -0500

    Add totalSupply rest api route

--- pages/api/totalsupply.js
+++ pages/api/totalsupply.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   try {
     const totalSupply = tokenContract.methods.totalSupply().call()
     console.log(`totalSupply :::: ${JSON.stringify(totalSupply)}`)
-    res.status(200).json({ totalSupply: totalSupply })
+    res.status(200).json({ totalSupply: JSON.stringify(totalSupply) })
   } catch(err) {
     res.status(500).json({ error: err.message })
   }
