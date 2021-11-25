998ac5adaa580d2f242fbfd1b5a08200feafd7e7
Jonathan Spruance <jspruance@inflightcorp.com>
2021-11-25 07:23:50 -0500

    Add totalSupply rest api route

--- pages/api/totalsupply.js
+++ pages/api/totalsupply.js
@@ -3,6 +3,7 @@ import tokenContract from '../../ethereum/token'
 export default function handler(req, res) {
   try {
     const totalSupply = tokenContract.methods.totalSupply().call()
+    console.log(`totalSupply :::: ${JSON.stringify(totalSupply)}`)
     res.status(200).json({ totalSupply: totalSupply })
   } catch(err) {
     res.status(500).json({ error: err.message })
